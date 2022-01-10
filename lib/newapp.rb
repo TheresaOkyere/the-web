c3c78c4407005ca56b9df7dfe201f66cefae380e
code-to <Theresaokyere034@gmail.com>
2022-01-10 16:28:54 +0000

    picture of cat

--- lib/newapp.rb
+++ lib/newapp.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
-# require "sinatra/reloader" if development?
+require "sinatra/reloader" if development?
 get '/' do
   'hello!'
 end
 
 get '/secret' do
   'helloT!'
+end
+
+get '/cat' do 
+  "<div>
+    <img src='http://placekitten.com/500/500'>
+   </div>"
 end
